4486e3de651435ecbcb93c6b109ce4280cfc621d
Sonam Vadsaria <sonamvadsaria@gmail.com>
2018-11-26 20:00:58 -0400

    Project Git Testing
    Master branch created. Project added

---
+++ gitTesting.java
@@ -0,0 +1,16 @@
+public class gitTesting
+{
+	
+	public static void main(String args[])
+
+	{
+	 int sum =add(2,3);
+
+	 System.out.println("sum of numbers:"+ sum);
+	}
+
+	public static int add(int x,int y)
+	{
+	return x+y;
+	}
+}
